9877b5f7ec34ebbbfec6288e04b4ea026f29c499
Handong Kim <aggsae@gmail.com>
2019-10-02 18:49:26 +0900

    기본 화면 세팅 완료, 문서 작업 이후 계속할 것

--- level1/javascript/loginFunction.js
+++ level1/javascript/loginFunction.js
@@ -1,3 +1,12 @@
+/* loginFunction.js */
+/* description */
+/* login 페이지에 적용되는 js 파일 */
+/* history */
+/* 20191001     0.0.0   html 코드와 분할    김한동 */
+/* 20191002     0.0.1   회원가입, ID/PW 찾기 페이지 이동 함수 추가  김한동 */
+
+/* :: TODO :: */
+
 function setDarkMode() {
     const date = new Date();
     const darkModeFlag = date.getHours();
@@ -12,15 +21,22 @@ function setDarkMode() {
 }
 
 function login() {
+
     const id = document.getElementsByName('id')[0].value;
     const pw = document.getElementsByName('password')[0].value;
 
-    // TODO :: 다음 페이지로 넘어가도록 설정
     if(id == "test" && pw == "test") {
         alert("login access");
-        // location.href="afterlogin.html";
         document.getElementById("loginfunction").submit();
     } else {
         alert("login fail");
     }
+}
+
+function moveToRegister() {
+    location.href="regUser.html";
+}
+
+function moveToFind() {
+    location.href="findIDPW.html";
 }
