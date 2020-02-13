ce27358bdaefd9ee37ab743e43077dbe21628868
201411108 <aggsae@gmail.com>
2020-02-13 18:41:20 +0900

    뷰 라우팅 테스트 작성, 프로젝트 구조 변경

--- sideproject/src/App.vue
+++ sideproject/src/App.vue
@@ -1,26 +1,50 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <PageTitle></PageTitle>
-    <PageBody></PageBody>
+    <!-- <PageTitle></PageTitle> -->
+    <!-- PageTitle 대신 nav 바로 대체 -->
+    <div id="nav">
+      <router-link to="/monthlyincome">Monthly Income</router-link> | 
+      <router-link to="/transactionalrecord">TransActinal Record</router-link>
+    </div>
+    <!-- PageBody 대신 PageBody 내용을 포함 -->
+    <!-- <PageBody></PageBody> -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <MonthlyIncome></MonthlyIncome>
+            </div>
+            <div class="col-md-4">
+                <TransactianlRecord></TransactianlRecord>
+            </div>
+            <div class="col-md-4">
+                <Account></Account>
+            </div>
+        </div>
+    </div>
+    <!-- TODO :: 세부 항목 보여주기 -->
+    <router-view/>
     <PageFooter></PageFooter>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import PageTitle from './components/PageTitle.vue'
-import PageBody from './components/PageBody.vue'
+// import PageTitle from './components/PageTitle.vue'
+// import PageBody from './components/PageBody.vue'
+import MonthlyIncome from './components/PageContents/MonthlyIncome.vue'
+import TransactianlRecord from './components/PageContents/TransactianalRecord.vue'
+import Account from './components/PageContents/Account.vue'
 import PageFooter from './components/PageFooter.vue'
 
 export default {
   name: 'app',
   components: {
-    // HelloWorld,
-    PageTitle,
-    PageBody,
-    PageFooter
+    // PageTitle,
+    // PageBody,
+    PageFooter,
+    MonthlyIncome,
+    TransactianlRecord,
+    Account
   }
 }
 </script>
--- sideproject/src/main.js
+++ sideproject/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import Test from './components/PageContents/test.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Router)
+
+const routes = [
+  {
+    path: '/monthlyincome',
+    component: Test
+  },
+  {
+    path: '/transactionalrecord',
+    component: Test
+  }
+]
+
+const router = new Router({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
