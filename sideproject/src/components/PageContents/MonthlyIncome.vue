d85fafb4e55403e9870c2e8348509ea532229ea8
201411108 <aggsae@gmail.com>
2020-02-13 16:24:59 +0900

    월별 수익 테스트 데이터로 작성, 나머지 todo 항목 확인

--- sideproject/src/components/PageContents/MonthlyIncome.vue
+++ sideproject/src/components/PageContents/MonthlyIncome.vue
@@ -1,15 +1,39 @@
 <template>
-    <p>
-        This is monthly income
-    </p>
+    <div id="monthlyIncome" class="container-fluid">
+        <h4>Monthly Income</h4>
+        <table class="table table-bordered">
+            <th id="sort">Month</th>
+            <th id="sort">Income</th>
+            <!-- TODO :: 최신 5개만 가져오도록 코드 수정 -->
+            <tr v-for="(income, month) in rawMonthIncome" v-bind:key="month">
+                <td>{{ month }}</td>
+                <td>{{ income }}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'monthlyIncome',
+    data() {
+        return {
+            // TODO :: 데이터를 어떻게 가져올 것인가 -> Vuex?
+            rawMonthIncome: {
+                '3월' : '300만',
+                '4월' : '400만',
+                '5월' : '450만',
+                '6월' : '500만',
+                '7월' : '600만',
+                '8월' : '700만'
+            },
+        }
+    },
 }
 </script>
 
-<style>
-
+<style scoped>
+#sort {
+    text-align: center;
+}
 </style>
--- sideproject/src/components/PageContents/TransactianalRecord.vue
+++ sideproject/src/components/PageContents/TransactianalRecord.vue
@@ -1,15 +1,41 @@
 <template>
-    <p>
-        This is Transactinal Record
-    </p>
+    <div id="transactionalRecord" class="container-fluid">
+        <h4>Transactional Record</h4>
+        <table class="table table-bordered">
+            <th id="sort">거래처</th>
+            <th id="sort">날짜</th>
+            <th id="sort">품목</th>
+            <th id="sort">부위</th>
+            <th id="sort">환자명</th>
+            <th id="sort">단가</th>
+            <th id="sort">수량</th>
+            <th id="sort">금액</th>
+            <th id="sort">접수 날짜</th>
+            <!-- TODO :: 최신 5개만 가져오도록 코드 수정 -->
+            <!-- <tr v-for="(income, month) in rawMonthIncome" v-bind:key="month">
+                <td>{{ month }}</td>
+                <td>{{ income }}</td>
+            </tr> -->
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'transactionalRecord',
+    data() {
+        return {
+            // TODO :: 데이터를 어떻게 가져올까? 배열로 한다면 v-for가 아니라 함수로 해야할 것 같다.
+            rawdata: {
+
+            }
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+#sort {
+    text-align: center;
+}
 </style>
