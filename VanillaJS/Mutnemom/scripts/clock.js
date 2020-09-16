84246b57cd2575b9054f84e39a7fa8960e098fee
Handong Kim <aggsae@gmail.com>
2020-09-16 23:03:11 +0900

    Add date

--- VanillaJS/Mutnemom/scripts/clock.js
+++ VanillaJS/Mutnemom/scripts/clock.js
@@ -1,5 +1,28 @@
-const clockContainer = document.querySelector(".js-time");
-const clockTitle = document.querySelector("h1");
+// REFACTOR :: js-clock 내 태그 변경 시 바뀌지 않도록 해야 함
+const H1_CLOCK = 0;
+const H3_CLOCK = 1;
+
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.children[H1_CLOCK];
+const dateTitle = clockContainer.children[H3_CLOCK];
+
+const dayConvertor = (dayOfWeek) => {
+  let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  return days[dayOfWeek];
+}
+
+const setDate = () => {
+  const calendar = new Date();
+  const year = calendar.getFullYear();
+  const month = calendar.getMonth();
+  const date = calendar.getDate();
+  let dayOfWeek = calendar.getDay();
+
+  dayOfWeek = dayConvertor(dayOfWeek);
+
+  dateTitle.innerText = `${year}/${month < 10 ? `0${month}` : `${month}`}/${date < 10 ? `0${date}` : `${date}`} ${dayOfWeek}`;
+}
 
 const setClock = () => {
   const date = new Date();
@@ -11,6 +34,7 @@ const setClock = () => {
 
 function init() {
   setClock();
+  setDate();
   // TODO :: 분마다 불러오기
   setInterval(setClock, 1000);
 }
